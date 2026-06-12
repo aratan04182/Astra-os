f854fef5bd7222d89495fefca9d7370d42401035
aratan04182 <aratan04182@gmail.com>
2026-06-13 06:01:31 +0900

    Create Brain.py

---
+++ Ai/Brain.py
@@ -0,0 +1,12 @@
+class Brain:
+
+    def __init__(self):
+
+        self.name = "AstraAI"
+        self.version = "0.1"
+
+    def run(self):
+
+        print(
+            f"{self.name} Version {self.version} Running..."
+        )
