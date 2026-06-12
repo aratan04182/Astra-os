ee9639fdcaeb57670c1b328d3b07bb01e4ef7c93
aratan04182 <aratan04182@gmail.com>
2026-06-13 07:28:55 +0900

    Create Planner.py

---
+++ Planner.py
@@ -0,0 +1,15 @@
+class Planner:
+
+    def create_plan(self, goal):
+
+        plan = []
+
+        plan.append("Analyze Goal")
+
+        plan.append("Search Memory")
+
+        plan.append("Generate Solution")
+
+        plan.append("Execute")
+
+        return plan
